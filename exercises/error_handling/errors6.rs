cafea3688c460d2892437d6d36d161b10c1a6e6d
flaymes <flaymes@outlook.com>
2024-04-25 21:56:34 +0800

    trait

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -32,10 +32,11 @@ impl ParsePosNonzeroError {
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x = match s.parse() {
-        Ok(val) => { val }
-        Err(err) => { return Err(ParsePosNonzeroError::ParseInt(err)); }
-    };
+    // let x = match s.parse() {
+    //     Ok(val) => { val }
+    //     Err(err) => { return Err(ParsePosNonzeroError::ParseInt(err)); }
+    // };
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parseint)?;
 
 
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
